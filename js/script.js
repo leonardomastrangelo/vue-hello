97c2c96a4e7be725a9efff31b9e119f5fcd68d4b
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-27 15:19:47 +0200

    title and image

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      title : "welcome to vue js bitch <3",
-      img : "img/halloween.jpg"
-    }
+      title: "welcome to vue js bitch <3",
+      img: "img/halloween.jpg",
+      sizing: "display-1",
+      color: "cadetblue"
+    };
   }
-}).mount('#app')
+}).mount("#app");
